478b1730a99ec5d375d90a04f35b072eff392ad0
microhe <fengpanhe@gmail.com>
2019-01-22 09:52:49 +0800

    trw-s  MRF

--- startup.m
+++ startup.m
@@ -3,5 +3,4 @@ addpath(genpath('./lib/segbench/lib/matlab'))
 addpath(genpath('./lib/GeometricContext'))
 addpath(genpath('./lib/iccv07-train'))
 addpath(genpath('./lib/iccv2011'))
-addpath(genpath('./lib/TRW-S'))
 addpath(genpath('./src'));
