abb01eb4416ec9db8ad3bcc43119fc6c01ad3edb
Feng Panhe <fengpanhe@gmail.com>
2019-02-18 23:48:28 +0800

    generateSyntheticDataset.m 画图的坐标轴的y轴改为向下，wseg完成

--- src/syntheticDataset/generateSyntheticDataset.m
+++ src/syntheticDataset/generateSyntheticDataset.m
@@ -30,22 +30,28 @@ function res = generateSyntheticDataset(image_num, dest_dir_path)
     max_shape_num = 10;
 
     image_size = [1200, 800];
+    wseg_matix_indices = (1:(image_size(1) * image_size(2)))';
+    [wseg_matix_pos_x, wseg_matix_pos_y] = ind2sub(image_size, wseg_matix_indices);
+    
 
-    set(0, 'DefaultFigureVisible', 'off');
+    % set(0, 'DefaultFigureVisible', 'off');
 
     for i = 1:image_num
         %% 生成 imgae_num 个 image 和 对应的 gt.mat
 
         im_name = sprintf('sd%d', i);
         im_file = fullfile(im_dir_path, strcat(im_name, '.jpg'));
-        gt_file = fullfile(im_dir_path, strcat(im_name, '_gt.mat'));
+        gt_file = fullfile(gt_dir_path, strcat(im_name, '_gt.mat'));
 
         edges_position = {};
         junctions_position = [0, 0];
+        wseg_matix = zeros(image_size);
 
         % 设置画布大小。plot 标定画布的角点，可避免坐标系自动调整
         figure('Color', 'w', 'Position', [0, 0, image_size]);
         set(gca, 'position', [0 0 1 1]);
+        set(gca,'YDir','reverse');
+        hold on;
         plot([0, 0, image_size(1), image_size(1)], [0, image_size(2), 0, image_size(2)], '.');
 
         shape_num = randi(max_shape_num);
@@ -64,6 +70,9 @@ function res = generateSyntheticDataset(image_num, dest_dir_path)
                 case 2
                     [shape_x, shape_y] = drawOneRandomRectangle([0, image_size(1)], [0, image_size(2)], rand(1, 3));
             end
+
+            [wseg_matix_pos_in, ~] = inpolygon(wseg_matix_pos_x, wseg_matix_pos_y, shape_x, shape_y);
+            wseg_matix(wseg_matix_pos_in) = j;
 
             edges_num = numel(edges_position);
             edges_position_remove_indexs = [];
@@ -119,20 +128,42 @@ function res = generateSyntheticDataset(image_num, dest_dir_path)
             junctions_position = [junctions_position; [shape_junctions_x, shape_junctions_y]];
         end % 画出图形的处理 end
 
+        bndinfo.edges.indices = posCell2indCell(image_size, edges_position);
+        bndinfo.imname = strcat(im_name, '.png');
+        bndinfo.imsize = image_size([2,1]);
+        bndinfo.junctions.position = round([junctions_position( :, 1), junctions_position(:,2)]);
+        bndinfo.wseg = transpose(wseg_matix);
+        save(gt_file, 'bndinfo');
+
         for ii = 1:numel(edges_position)
             hold on;
             plot(edges_position{ii}(:, 1), edges_position{ii}(:, 2), 'b', 'LineWidth', 2);
         end
-
+        hold on;
         mapshow(junctions_position(:, 1), junctions_position(:, 2), 'DisplayType', 'point', 'Marker', 'o');
 
-        axis off;
+        % axis off;
         print('-dpng', '-r0', [im_file(1:end - 4) '.png']);
-        close all;
+        % close all;
     end
 
     set(0, 'DefaultFigureVisible', 'on');
 
+end
+
+function ind_cell = posCell2indCell(xy_szie, pos_cell)
+    %posCell2indCell - Description
+    %
+    % Syntax: ind_cell = posCell2indCell(xy_szie, pos_cell)
+    %
+    % Long description
+    yx_size = xy_szie([2, 1]);
+    ind_cell = cell(size(pos_cell));
+    for i = 1:numel(pos_cell)
+        pos_x = round(pos_cell{i}(:, 1));
+        pos_y = round(pos_cell{i}(:, 2));
+        ind_cell{i} = sub2ind(yx_size, pos_y, pos_x);
+    end
 end
 
 function [output_points_x, output_points_y] = sortPoints(points_x, points_y, vx, vy)
@@ -290,6 +321,7 @@ function [shape_x, shape_y] = drawOneRandomTriangle(x_range, y_range, shape_colo
     shape_x(end) = shape_x(1);
     shape_y(end) = shape_y(1);
     % patch(shape_x, shape_y, shape_color, 'FaceColor', 'none');
+    hold on;
     patch(shape_x, shape_y, shape_color);
 end
 
@@ -321,5 +353,6 @@ function [shape_x, shape_y] = drawOneRandomRectangle(x_range, y_range, shape_col
     shape_y(end) = shape_y(1);
 
     % patch(shape_x, shape_y, shape_color, 'FaceColor', 'none');
+    hold on;
     patch(shape_x, shape_y, shape_color);
 end
