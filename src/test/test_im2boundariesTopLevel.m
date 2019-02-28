fc2028f973fe0fa5bf944bbc72f01f47b2c696cf
Feng Panhe <fengpanhe@gmail.com>
2019-02-28 22:19:39 +0800

    测试修改

--- src/test/test_im2boundariesTopLevel.m
+++ src/test/test_im2boundariesTopLevel.m
@@ -1,8 +1,10 @@
-imname = 'house05';
+imname = 'sd3';
+image_file = strcat('resources/SyntheticDataset/images/', imname, '.jpg');
 
-im = imread(['resources/GeometricContextDataset/images/' imname '.jpg']);
+im = imread(image_file);
 [bndinfo, pbim, gconf, bndinfo_all] = im2boundariesTopLevel(double(im) / 255);
-saldir = './result/';
-savePath = [saldir imname '_seg.mat'];
-% save(savePath,'bndinfo', 'pbim', 'gconf', 'bndinfo_all');
-iccvWriteContourDepthResults();
+bndinfo_file = fullfile('result', strcat(imname, '_ex.mat'));
+save(bndinfo_file, 'bndinfo', 'pbim', 'gconf', 'bndinfo_all');
+
+dest_dir_path = fullfile('result');
+writeContourDepthResults(bndinfo_file, image_file, dest_dir_path, 1);
