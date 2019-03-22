1d326ab512a38d6628a0d1bc1c261a0ab7c4da33
Panhe <fengpanhe@gmail.com>
2019-03-22 15:52:42 +0800

    add lib/vlfeat/*

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # cvpr2012
 论文 A learning-based framework for depth ordering 的复现
 
+## 依赖准备
+
+### 1. vlfeat
+
+下载 [VLFeat](http://www.vlfeat.org/download/vlfeat-0.9.21-bin.tar.gz) 并解压到目录 `lib/vlfeat`
+
+
 ## Synthetic Dataset
 
 1. 生成数据集 10 张
--- setpath.m
+++ setpath.m
@@ -1,6 +1,7 @@
-addpath(genpath('./lib/lightspeed-master'))
-addpath(genpath('./lib/segbench/lib/matlab'))
-addpath(genpath('./lib/GeometricContext'))
-addpath(genpath('./lib/iccv07-train'))
-addpath(genpath('./lib/iccv2011'))
+run('lib/vlfeat/toolbox/vl_setup');
+addpath(genpath('./lib/lightspeed-master'));
+addpath(genpath('./lib/segbench/lib/matlab'));
+addpath(genpath('./lib/GeometricContext'));
+addpath(genpath('./lib/iccv07-train'));
+addpath(genpath('./lib/iccv2011'));
 addpath(genpath('./src'));
